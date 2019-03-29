5101afc0e5f1d2a3022bd95ba58ca5634c4ba388
Salman <rahman_5@live.ca>
2019-03-29 10:51:51 -0400

    Added Softwares and Statistics Nodes in the database

--- pythonWebApps/testFlaskApp/serverSideComputation.py
+++ pythonWebApps/testFlaskApp/serverSideComputation.py
@@ -64,11 +64,12 @@ def updateNumberOfPeopleAvailabilitySpots(numberOfPeople, privateKeyPath):
     })
 
 
-def storeSoftwareLabs2DB():
+def storeSoftwareLabs2DB(privateKeyPath):
     softLabDict = softwareParser.softwareParsingMain()
 
     # ============== Test Private key path =========================
-    privateKeyPath = "engrlabs-10f0c-firebase-adminsdk-oswwf-ebef7d1bf1.json"
+    # privateKeyPath = "engrlabs-10f0c-firebase-adminsdk-oswwf-ebef7d1bf1.json"
+    # ============== Test Private key path =========================
 
     # storing the data 2 db
     ref = connectAndCreateBlankCollectionAndDB(privateKeyPath)
@@ -83,34 +84,99 @@ def storeSoftwareLabs2DB():
 
         listOflabs = list((dict(softLabDict).get(softKey)))
 
-        # print softKey
+        ref_softwares.update({
+            unicode(str(softKey)): {
+                "Labs": {}
+            }
+        })
+
+        ref_Labs = ref.child('PUBLIC_DATA/Softwares/' + str(softKey) + "/" + "Labs")
 
         for lab in listOflabs:
             # print str(lab.softwareName)
             # print str(lab.classBuilding) + str(lab.classFloor) + str(lab.classRoom)
             # print unicode(str(softKey))
+            pause = 0
 
-            # print lab.softwareName
-
-            tempSoft = {
-                unicode(str(softKey)): {
-                    unicode(str(unicode(str(lab.classBuilding) + str(lab.classFloor) + str(lab.classRoom)))): {
-                        unicode("Room"): unicode(str(lab.classFloor) + str(lab.classRoom)),
-                        unicode("RoomCode"): unicode(str(lab.classBuilding) + str(lab.classFloor) + str(lab.classRoom)),
-                        unicode("BuildingCode"): unicode(str(lab.classBuilding)),
-                        unicode("floor"): unicode(str(lab.classFloor))
-                    }
+            ref_Labs.update({
+                unicode(str(unicode(str(lab.classBuilding) + str(lab.classFloor) + str(lab.classRoom)))): {
+                    unicode("Room"): unicode(str(lab.classFloor) + str(lab.classRoom)),
+                    unicode("RoomCode"): unicode(str(lab.classBuilding) + str(lab.classFloor) + str(lab.classRoom)),
+                    unicode("BuildingCode"): unicode(str(lab.classBuilding)),
+                    unicode("floor"): unicode(str(lab.classFloor))
                 }
-            }
-
-            dict(tempSoft).
-
-    pause = 0
+            })
 
 
-            # ref_softwares.update({
+def dynamicDataDictFunc(privateKeyPath):
+    ref = connectAndCreateBlankCollectionAndDB(privateKeyPath)
+    # ref_allData = ref.child('PUBLIC_DATA')
+    ref_DynamicData = ref.child('PUBLIC_DATA/DynamicData')
+    try:
+        dynamicDataDict = dict(ref_DynamicData.get())
 
-            # })
+        # print(type(ref_DynamicData.get()))
+
+        return dynamicDataDict, ref
+
+    except:
+        return None
+
+
+
+
+def calculateTotalCapacity(privateKeyPath, dynamicDataDict, ref):
+    if dynamicDataDict is None:
+        print "Error Retrieving from database"
+        return
+
+    totalCapacity = 0
+    labKeys = dynamicDataDict.keys()
+    for labKey in labKeys:
+        labCapacity = dict(dynamicDataDict.get(labKey)).get("TotalCapacity")
+        # print labCapacity
+        try:
+            labCapacityInt = int(labCapacity)
+            totalCapacity = totalCapacity + labCapacityInt
+        except:
+            print "Error Converting Capacity"
+            return
+
+    ref_statistics = ref.child('PUBLIC_DATA/Statistics')
+
+    ref_statistics.update({
+        unicode("Total_Capacity"): unicode(str(totalCapacity))
+    })
+
+    # print totalCapacity
+
+    debuggerPause = 0
+
+def calculateTotalNumberOfStudents(privateKeyPath, dynamicDataDict, ref):
+    if dynamicDataDict is None:
+        print "Error Retrieving from database"
+        return
+
+    totalNumberOfStudents = 0
+    labKeys = dynamicDataDict.keys()
+    for labKey in labKeys:
+        totalStudentsPresent = dict(dynamicDataDict.get(labKey)).get("NumberOfStudentsPresent")
+        # print totalStudentsPresent
+        try:
+            print type(totalStudentsPresent)
+            print str(totalNumberOfStudents).isdigit()
+            # totalNumberInt = int(totalStudentsPresent)
+            # totalNumberOfStudents = totalNumberOfStudents + totalNumberInt
+        except:
+            print "Error Converting Total Students Present"
+            return
+
+    ref_statistics = ref.child('PUBLIC_DATA/Statistics')
+
+    ref_statistics.update({
+        unicode("Total_NumberOfStudentsPresent"): unicode(str(totalNumberOfStudents))
+    })
+    debuggerPause = 0
 
 
 def serverSideComputationTest(numberOfPeople):
@@ -126,6 +192,12 @@ def serverSideComputationTest(numberOfPeople):
 
 # ***** Main ******
 # serverSideComputation()
-storeSoftwareLabs2DB()
+
+privateKeyPath = "engrlabs-10f0c-firebase-adminsdk-oswwf-ebef7d1bf1.json"
+dynamicDataDict, ref = dynamicDataDictFunc(privateKeyPath)
+calculateTotalCapacity(privateKeyPath, dynamicDataDict, ref)
+calculateTotalNumberOfStudents(privateKeyPath, dynamicDataDict, ref)
+# storeSoftwareLabs2DB(privateKeyPath)
+
 
 # ***** END ******
